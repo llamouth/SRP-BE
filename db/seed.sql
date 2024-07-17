0a7ca06091d4a4942d341e87006b1d2bddbb0825
llamouth <150949923+llamouth@users.noreply.github.com>
2024-07-17 17:04:09 -0400

    Update back-end

---
+++ db/seed.sql
@@ -0,0 +1,28 @@
+\c cafe_dev;
+
+INSERT INTO products (product_name, product_price, product_quantity) VALUES 
+('coffee', 400, 20),
+('bagel', 100, 20),
+('butter roll', 100, 10),
+('snicker', 100, 10),
+('crossaint', 100, 15),
+('bread', 100, 10),
+('cookie', 100, 10),
+('coke', 100, 10),
+('snapple', 200, 10),
+('pepsi', 250, 15);
+
+INSERT INTO users (user_name, user_address) VALUES 
+('Owner', 'Cafe'),
+('John Doe', '123 abc st'),
+('Jane Doe', '789 xyz ave'),
+('Bob', '456 mno dr');
+
+INSERT INTO cart (cart_owner, cart_products) VALUES 
+(3, '1-2, 2-1, 3-3'),
+(2, '10-2, 5-1, 6-3'),
+(4, '3-1, 6-3, 2-1');
+
+INSERT INTO orders (order_user, order_cart) VALUES 
+(3 , 1),
+(4 , 3);
---
+++ queries/products.js
@@ -0,0 +1,9 @@
+const db = require("../db/dbConfig")
+
+const getAllProducts = () => {
+    try {
+        const allProducts = db.one
+    } catch (error) {
+        return error
+    }
+}
