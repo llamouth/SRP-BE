026a78180adaa8583b18ea54a82e96a7e605c75e
CSAN7690 <christophersanchez@pursuit.org>
2024-07-22 11:31:19 -0400

    SignUp Route update

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -5,10 +5,10 @@ const { getAllUsers, getOneUser, createUser, updateUser, deleteUser } = require(
 users.get("/", async (req, res) => {
     const allUsers = await getAllUsers()
 
-    if(allUsers) {
+    if (allUsers) {
         res.status(200).json(allUsers)
-    }else {
-        res.status(500).json({ error : "Internal Server Error"})
+    } else {
+        res.status(500).json({ error: "Internal Server Error" })
     }
 })
 
@@ -16,12 +16,30 @@ users.get("/:id", async (req, res) => {
     const { id } = req.params
     const oneUser = await getOneUser(id)
 
-    if(oneUser.user_id){
+    if (oneUser.user_id) {
         res.status(200).json(oneUser)
-    }else {
-        res.status(500).json({ error: "Internal Server Error"})
+    } else {
+        res.status(500).json({ error: "Internal Server Error" })
     }
 })
+
+// Signup Route
+users.post("/signup", async (req, res) => {
+    const { username, password, address } = req.body;
+    const allUsers = await getAllUsers();
+    const userExists = allUsers.find(user => user.user_name === username);
+    if (userExists) {
+        return res.status(400).json({ message: "User already exists" });
+    }
+
+    const newUser = await createUser({ user_name: username, user_password: password, user_address: address });
+    if (newUser) {
+        res.status(201).json({ message: "User registered successfully", user: newUser });
+    } else {
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+});
+
 
 users.post("/", async (req, res) => {
     const newUser = await createUser(req.body)
@@ -32,10 +50,10 @@ users.put("/:id", async (req, res) => {
     const { id } = req.params
     const updatedUser = await updateUser(id, req.body)
 
-    if(updatedUser.user_id){
+    if (updatedUser.user_id) {
         res.status(200).json(updatedUser)
-    }else {
-        res.status(500).json({ error: "Internal Server Error"})
+    } else {
+        res.status(500).json({ error: "Internal Server Error" })
     }
 })
 
